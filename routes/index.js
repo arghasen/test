63b72e7026d8d03361a371311466f71225b4dbf4
Argha Sen <iamslowdeath@gmail.com>
2019-09-08 15:04:19 +0800

    demo ready

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,25 @@ var data =["Hello, Welcome to Venniset Demo","This is an encrypted Vault","Store
 var encryptedData =["uMzhdHO12ypQi21yPfNgKj5TD2MHxNUoc8vNiebm+0Q=","myX/C8IgIFARtJA3I4bO6FuWeIt1Bjk21Lr6Yb6x2xU=","OTLIl0XA7viWKW8LEG6LHbgr2GT+axxJ7RL7185CaVE="]
 var vaultStatus=0
 var JuanKey = "abc123"
-var alexKey = "abcd"
-var ivykey ="abcd"
-var benkey ="abcd"
+var trustedKeys =["alex","ivy", "ben"]
+var trustedParties=[0,0,0]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // request('https://www.simcompanies.com/api/v1/market-ticker/2019-08-06T03:02:00.000Z/', { json: true }, (err, resp, body) => {
   // if (err) { return console.log(err); }
   // console.log(body)
+  if(trustedParties[0]+trustedParties[1]+trustedParties[2]==2)
+    vaultStatus = 1
+
   console.log(vaultStatus)
+
   var sendingData
   if(vaultStatus==1)
     sendingData=data
   else
     sendingData=encryptedData
-  res.render('index', {  data:sendingData, vaultStatus:vaultStatus });
+ 
+  res.render('index', {  data:sendingData, vaultStatus:vaultStatus,trustedParties:trustedParties});
   // });
 });
 
@@ -29,7 +33,7 @@ router.get('/trusted', function(req, res, next) {
   // request('https://www.simcompanies.com/api/v1/market-ticker/2019-08-06T03:02:00.000Z/', { json: true }, (err, resp, body) => {
   // if (err) { return console.log(err); }
   // console.log(body)
-  res.render('trusted', { });
+  res.render('trusted', { status:0});
   // });
 });
 router.get('/unlocked', function(req, res, next) {
@@ -45,7 +49,23 @@ router.get('/locked', function(req, res, next) {
   // if (err) { return console.log(err); }
   // console.log(body)
   vaultStatus = 0
+  trustedParties=[0,0,0]
   res.redirect('/')
+  // });
+});
+router.get('/trustedOpen', function(req, res, next) {
+  // request('https://www.simcompanies.com/api/v1/market-ticker/2019-08-06T03:02:00.000Z/', { json: true }, (err, resp, body) => {
+  // if (err) { return console.log(err); }
+  // console.log(body)
+  n =req.query["usrname"]
+  if (n=="alex")
+    trustedParties[0] =1
+  else if (n=="ivy")
+    trustedParties[1] = 1
+  else if (n=="ben")
+    trustedParties[2] = 1
+
+  res.render('trusted',{status:1})
   // });
 });
 module.exports = router;
